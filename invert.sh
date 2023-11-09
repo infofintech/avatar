d28989cd8fe39824e27b53a2b37b32a7aed9c4c8
Karl August Nestor <jesuscaesar@outlook.com>
2023-11-09 23:37:47 +0000

    Updated package metadata

--- invert.sh
+++
@@ -1,2 +0,0 @@
-for file in iso.*.png; do convert $file -channel RGB -negate ${file//iso/iec}; echo $file; done
-for file in ava.*.png; do convert $file -channel RGB -negate ${file//ava/abc}; echo $file; done
